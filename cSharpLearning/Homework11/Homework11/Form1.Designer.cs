cdaceb72e1a6e3c2671bad7a587d5afe80009e2d
Chenliang Xing <57756451+galaxyview@users.noreply.github.com>
2022-10-18 13:14:59 +0800

    Add files via upload

---
+++ cSharpLearning/Homework11/Homework11/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Homework11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(75, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 52);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(75, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 58);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "下一个";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "请输入";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 318);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ cSharpLearning/Homework11/Homework11/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Homework11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        Random rand = new Random();
+        int i;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            label2.Text = "请输入";
+            //建立链接
+            string connString = "Data Source = words.db";//数据库的数据格式为Num，Chinese，English
+            SQLiteConnection connection = new SQLiteConnection(connString);
+            connection.Open();
+            
+            //从数据库中随机抽取一个单词
+            i = rand.Next(1, 5);
+            string sql = "SELECT * FROM WORDS WHERE Num = "+i+";";
+
+            //运用command和datareader读取数据库中的数据
+            SQLiteCommand command = new SQLiteCommand(sql, connection);
+            SQLiteDataReader dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                label1.Text = (string)dataReader["Chinese"];
+            }
+            dataReader.Close();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //建立链接
+                string connString = "Data Source = words.db";
+                SQLiteConnection connection = new SQLiteConnection(connString);
+                connection.Open();
+
+                //从数据库中随机抽取一个单词
+                string str = "";
+                string sql = "SELECT * FROM WORDS WHERE Num = " + i + ";";
+
+                //运用command和datareader读取数据库中的数据
+                SQLiteCommand command = new SQLiteCommand(sql, connection);
+                SQLiteDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    str = (string)dataReader["English"];
+                }
+                dataReader.Close();
+
+                //判断输入单词是否正确
+                if (str == textBox1.Text)
+                {
+                    label2.Text = "正确";
+                }
+                else
+                {
+                    label2.Text = "错误";
+                    textBox1.Text = str;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
