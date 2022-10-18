d0d8111dba36c8a371de49c48a52b7656903bfe7
Chenliang Xing <57756451+galaxyview@users.noreply.github.com>
2022-10-18 13:02:26 +0800

    Add files via upload

---
+++ cSharpLearning/Homework5/Homework5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework5
+{
+    
+    class Program
+    {
+        public bool checkPrime(int num)
+        {
+            for (int j = 2; j < num; j++)
+            {
+                if (num % j == 0) return false;
+            }
+            return true;
+        }
+
+        public void ChangeIntoPrime(int num)
+        {
+            var numbers = Enumerable.Range(1, (int)(num / 2) + 1);
+            var PrimeNumbers = from w in numbers where checkPrime(w) && checkPrime(num-w) select w;
+            foreach(var n in PrimeNumbers)
+            {
+                Console.WriteLine(n + "+" + (num - n) + "=" + num);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            var numbers = Enumerable.Range(6, 100);
+            var EvenNumbers = from n in numbers where n % 2 == 0 select n;
+            foreach(var n in EvenNumbers)
+            {
+                p.ChangeIntoPrime(n);
+            }
+        }
+    }
+}
