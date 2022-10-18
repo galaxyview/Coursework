9efc54c22c6300653f5217fa20f62dccb488ea61
Chenliang Xing <57756451+galaxyview@users.noreply.github.com>
2022-10-18 13:20:53 +0800

    Add files via upload

---
+++ Windows/WindowsHomework-csDll/WindowsHomework-csDll/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WindowsHomework_csDll
+{
+    public class Class1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Decline(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
