1c69abf1934433816481c06d43d443da55eb00b1
Chenliang Xing <57756451+galaxyview@users.noreply.github.com>
2022-10-18 13:08:47 +0800

    Add files via upload

---
+++ cSharpLearning/Homework9/Homework9/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Homework9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(446, 38);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "验证";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "身份证号：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, -208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(665, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 31);
+            this.label3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(483, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "重置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 178);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "身份证号码正确性检验";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ cSharpLearning/Homework9/Homework9/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Homework9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tx = textBox1.Text;
+            bool flag = false;
+            flag = Regex.IsMatch(tx, @"(\d){17}(\d|X|x)");
+            if (flag)
+            {
+                if (isLegal(tx))
+                {
+                    label3.Text = "验证通过";
+                }
+                else
+                {
+                    label3.Text = "验证未通过";
+                }
+            }
+            else
+            {
+                label3.Text = "验证未通过";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            label3.Text = null;
+        }
+
+        public bool isLegal(string IDNum)
+        {
+            int[] matches = new int[17];
+            for(int i = 0; i < 17; i++)
+            {
+                matches[i] = IDNum[i] - 48;
+            }
+            int sum = 7 * matches[0] + 9 * matches[1] + 10 * matches[2] + 5 * matches[3] + 8 * matches[4] + 4 * matches[5] + 2 * matches[6] + matches[7] + 6 * matches[8] + 3 * matches[9] + 7 * matches[10] + 9 * matches[11] + 10 * matches[12] + 5 * matches[13] + 8 * matches[14] + 4 * matches[15] + 2 * matches[16];
+            int remainder = sum % 11; //34052419800101001x
+            char legal = '\n';
+            switch (remainder)
+            {
+                case 0: legal = '1'; break;
+                case 1: legal = '0'; break;
+                case 2: legal = 'X'; break;
+                case 3: legal = '9'; break;
+                case 4: legal = '8'; break;
+                case 5: legal = '7'; break;
+                case 6: legal = '6'; break;
+                case 7: legal = '5'; break;
+                case 8: legal = '4'; break;
+                case 9: legal = '3'; break;
+                case 10: legal = '2'; break;
+            }
+            if (IDNum[17] == legal)
+            {
+                return true;
+            }
+            else
+            {
+                if((IDNum[17]=='x'|| IDNum[17] == 'X') && legal == 'X')
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+}
