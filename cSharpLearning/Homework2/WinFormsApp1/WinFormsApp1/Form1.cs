69d18704c21d9de291024355c60539ee241cc8ea
Chenliang Xing <57756451+galaxyview@users.noreply.github.com>
2022-10-18 12:57:53 +0800

    Add files via upload

---
+++ cSharpLearning/Homework2/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int max, min,temp,count;
+        bool flag;
+
+        public bool isnumeric(string str)
+        {
+            char[] ch = new char[str.Length];
+            ch = str.ToCharArray();
+            for (int i = 0; i<str.Length;i++  )  {
+                if (ch[i] < 48 || ch[i] > 57)
+                    return false;
+            }
+            return true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            richTextBox1.Text = "";
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Text.RegularExpressions.Regex rex =
+        new System.Text.RegularExpressions.Regex(@"^\d+$");
+            if (rex.IsMatch(textBox2.Text) &&rex.IsMatch(textBox2.Text))
+            {
+                richTextBox1.Text = "";
+                max = int.Parse(textBox1.Text);
+                min = int.Parse(textBox2.Text);
+                for (int i = min + 1; i < max; i++)
+                {
+                    temp = 1;
+                    count = 0;
+                    flag = false;
+                    while (temp <= i / 2 + 1)
+                    {
+                        if (count >= 2)
+                        {
+                            flag = true;
+                            break;
+                        }
+                        if (i % temp == 0)
+                        {
+                            count++;
+                        }
+                        temp++;
+                    }
+                    if (flag == false)
+                    {
+                        richTextBox1.Text += i.ToString() + " ";
+                    }
+                }
+            }
+            else
+            {
+                richTextBox1.Text = "输入有误,请重新输入！";
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+    }
+}
+
+
---
+++ cSharpLearning/Homework2/myapp/myapp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a;
+            int MAX, MIN, SUM = 0;
+            string s;
+            a = new int[10];
+            for(int i = 0;i < 10; i++)
+            {
+                Console.Write("Input No."+i+"\n");
+                s = Console.ReadLine();
+                a[i] = int.Parse(s);
+            }
+            MAX = a[0];
+            MIN = a[0];
+            for (int i = 1;i < 10; i++)
+            {
+                if (a[i] > MAX)
+                {
+                    MAX = a[i];
+                }
+                else
+                {
+                    if (a[i] < MIN)
+                    {
+                        MIN = a[i];
+                    }
+                }
+                SUM += a[i];
+            }
+            Console.Write("The max number is:" + MAX+"\n");
+            Console.Write("The min number is:" + MIN + "\n");
+            Console.Write("The average number is:" + SUM/10 + "\n");
+            Console.Write("The sum is:" + SUM + "\n");
+
+
+        }
+    }
+    
+}
